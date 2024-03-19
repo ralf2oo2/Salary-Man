4fc4341643f77b780de60cbe68c5ef9c68aabe2d
Genarovw <genaro.vanwoesik@hotmail.com>
2024-03-19 13:54:17 +0100

    Added one way patrolling

---
+++ Assets/Scripts/AI/EnemyPatrolling.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolling : MonoBehaviour
+{
+    public Transform[] points;
+    int current;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position != points[current].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, points[current].position, speed);
+        }
+    }
+}
