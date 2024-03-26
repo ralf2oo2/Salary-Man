5e825e1badcfb2a6c5b084bc560f604c39a105a7
ralf2oo2 <ralf2oo2@hotmail.com>
2024-03-26 14:23:35 +0100

    Added animations and a few states

---
+++ Assets/Scripts/AI/Enemy/States/ChaseState.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ChaseState : EnemyBaseState
+{
+    GameObject player;
+    public ChaseState(Enemy enemy, Animator animator) : base(enemy, animator)
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public override void OnEnter()
+    {
+        animator.CrossFade(runFwdHash, crossFadeDuration);
+        enemy.Agent.speed = 3;
+    }
+
+    public override void FixedUpdate()
+    {
+
+    }
+
+    public override void OnExit()
+    {
+
+    }
+
+    public override void Update()
+    {
+        Debug.Log("teest");
+        enemy.Agent.SetDestination(player.transform.position);
+    }
+}
---
+++ Assets/Scripts/AI/Enemy/States/IdleState.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class IdleState : EnemyBaseState
+{
+    public IdleState(Enemy enemy, Animator animator) : base(enemy, animator)
+    {
+        
+    }
+
+    public override void OnEnter()
+    {
+        animator.CrossFade(idleHash, crossFadeDuration);
+    }
+
+    public override void FixedUpdate()
+    {
+
+    }
+
+    public override void OnExit()
+    {
+
+    }
+
+    public override void Update()
+    {
+
+    }
+}
