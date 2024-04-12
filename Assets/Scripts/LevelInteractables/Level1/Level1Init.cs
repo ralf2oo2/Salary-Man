221a86433bd13cf0aa1a31b6068b8b2ab08412dd
ralf2oo2 <ralf2oo2@hotmail.com>
2024-04-12 10:18:12 +0200

    Added patrol points

--- Assets/Scripts/LevelInteractables/Level1/Level1Init.cs
+++ Assets/Scripts/LevelInteractables/Level1/Level1Init.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class Level1Init : MonoBehaviour
 {
+    private bool placedCode = false;
     // Start is called before the first frame update
     void Start()
     {
         ObjectiveManager.SetObjective("Try to find a way into the building");
+    }
+
+    private IEnumerator SetCodeLocation()
+    {
+        yield return new WaitForSeconds(5);
         GameObject.FindGameObjectWithTag("Code").transform.position = CodeSpawnPoint.codeSpawnPoints[Random.Range(0, CodeSpawnPoint.codeSpawnPoints.Count - 1)].transform.position;
+    }
+
+    private void Update()
+    {
+        if(CodeSpawnPoint.codeSpawnPoints.Count > 0 && !placedCode)
+        {
+            placedCode = true;
+            StartCoroutine(SetCodeLocation());
+        }
     }
 }
